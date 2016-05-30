3d7aa5d73ed040cd11436dd7a7598b5e42abba2e
JAD3 <jade.getz@gmail.com>
2016-05-29 19:40:47 -0700

    Added Marker & Style Changes to jsMap
     Please enter the commit message for your changes. Lines starting

--- jsMap.js
+++ jsMap.js
@@ -1,12 +1,25 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-  /* Additional Map Code Here */ 
-  	  center: {
-      lat: 48.856614,
-      lng: 2.352222
-    },
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+var map;
+var marker;
 
-  });
- }
+
+function initMap() {
+    var myLatLng = {lat: 48.856614, lng: 2.352222};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    addMarker(myLatLng, "I\'m a marker!");
+}
+    
+    function addMarker(location, title){
+    marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: title
+    });
+}
+
+
